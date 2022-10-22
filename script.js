e946f24d081c9dab5ed022ec940e5c6ced18294b
DESKTOP-3TH5724\baske <basket100713hiroki.ch@gmail.com>
2022-10-22 11:50:09 +0900

    work・contact作成（CSSなし）

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 
-const mySwiper = new Swiper('.swiper', {
+//Swiper(スライダー)
+const swiper = new Swiper('.swiper', {
   // Optional parameters
-  loop: true,
- 
+  loop: false,//永遠にスライドする（1→2→3→1）
+  slidesPerView:1,
+
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination',//スライド分のドットを作れる
+  clickable:true //ドットをクリックするとページ遷移
   },
- 
+
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
- 
+
   // And if we need scrollbar
   scrollbar: {
     el: '.swiper-scrollbar',
