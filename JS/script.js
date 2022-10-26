dc29eab7c74e21212cf3ee63a957eba204b5b38b
DESKTOP-3TH5724\baske <basket100713hiroki.ch@gmail.com>
2022-10-26 21:27:27 +0900

    contact-form(非ajax)

--- JS/script.js
+++ JS/script.js
@@ -37,3 +37,26 @@ $(function() {
       $(".header-drawer-nav").removeClass('active');//ナビゲーションのactiveクラスも除去
   });
   });
+
+
+    //お問い合わせ完了処理
+    $('#form').submit(function (event) {
+      var formData = $('#form').serialize();
+      $.ajax({
+        url:"https://docs.google.com/forms/u/0/d/e/1FAIpQLSe69Aa16qm1TE3JXda8GBgqSEnadRKqKdufNVML7IXsKfdtaQ/formResponse",
+        data: formData,
+        type: "POST",
+        dataType: "xml",
+        statusCode: {
+          0: function () {
+            $(".end-message").slideDown();
+            $(".contact-submit-btn").fadeOut();
+            //window.location.href = "thanks.html";
+          },
+          200: function () {
+            $(".false-message").slideDown();
+          }
+        }
+      });
+      event.preventDefault();
+    });
